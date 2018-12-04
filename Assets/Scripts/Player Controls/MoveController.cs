380ab94f674476ed016e1c757c44f76d1e5e7361
HS @ FYP Lab <deathezer10@gmail.com>
2018-12-04 16:02:11 +0800

    Initial Hand and head tracking

---
+++ Assets/Scripts/Player Controls/MoveController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PS4.VR;
+
+
+public enum MoveControllerOrientation
+{
+    Left,
+    Right
+}
+
+public class MoveController : MonoBehaviour
+{
+
+    private MoveControllerOrientation m_Orientation;
+
+    private int m_HandleNumber = -1;
+
+    bool m_IsRegistered = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (m_IsRegistered)
+        {
+            Vector3 pos;
+            if (Tracker.GetTrackedDevicePosition(m_HandleNumber, out pos) == PlayStationVRResult.Ok)
+            {
+                transform.position = pos;
+            }
+
+            Quaternion rot;
+            if (Tracker.GetTrackedDeviceOrientation(m_HandleNumber, out rot) == PlayStationVRResult.Ok)
+            {
+                transform.rotation = rot;
+            }
+        }
+    }
+
+    public void RegisterController(MoveControllerOrientation orientation, int handleNumber)
+    {
+        m_IsRegistered = true;
+        m_HandleNumber = handleNumber;
+        m_Orientation = orientation;
+        Tracker.RegisterTrackedDevice(PlayStationVRTrackedDevice.DeviceMove, handleNumber, PlayStationVRTrackingType.Absolute, PlayStationVRTrackerUsage.Default);
+    }
+
+}
---
+++ Assets/Scripts/Player Controls/MoveControllerHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PS4;
+using UnityEngine.SpatialTracking;
+using UnityEngine.PS4.VR;
+
+public class MoveControllerHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private MoveController m_MovePrimaryController, m_MoveSecondaryController;
+
+    // Use this for initialization
+    void Start()
+    {
+        RegisterControllers();
+    }
+
+    void RegisterControllers()
+    {
+        int[] primaryHandles = new int[1];
+        int[] secondaryHandles = new int[1];
+
+        PS4Input.MoveGetUsersMoveHandles(1, primaryHandles, secondaryHandles);
+
+        int movePrimaryHandle = primaryHandles[0];
+        int moveSecondaryHandle = secondaryHandles[0];
+
+        m_MovePrimaryController.RegisterController(MoveControllerOrientation.Left, movePrimaryHandle);
+        m_MoveSecondaryController.RegisterController(MoveControllerOrientation.Right, moveSecondaryHandle);
+    }
+
+
+
+}
