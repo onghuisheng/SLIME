5fb2d170c90cae3153f91eca73a9923bfd5f60a0
marcuslim4299 <marcuslim4299@gmail.com>
2018-12-04 18:05:00 +0800

    Separate scene with a cube moving by navmesh and can jump

---
+++ Assets/Marcus Assets/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour {
+
+    public List<GameObject> m_pathList; // list of the path that the ai can travel
+
+    public int m_index; // index for which node the ai should travel to next
+
+	// Use this for initialization
+	void Start () {
+        m_index = 0;
+        GetComponent<NavMeshAgent>().SetDestination(m_pathList[m_index].transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (m_index < m_pathList.Count - 1)
+        {
+            if (Vector3.Distance(transform.position, m_pathList[m_index].transform.position) > 0.1f)
+            {
+                return;
+            }
+
+            m_index++;
+            GetComponent<NavMeshAgent>().SetDestination(m_pathList[m_index].transform.position);
+        }
+	}
+}
---
+++ Assets/Scripts/PS4MoveController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PS4;
+
+public class PS4MoveController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(PS4Input.MoveIsConnected(0, 0))
+        {
+            PS4Input.MoveSetLightSphere(0, 0, 1, 1, 1);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
