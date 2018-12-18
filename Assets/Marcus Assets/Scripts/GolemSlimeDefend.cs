743cb572a7d2605476402658190885891e149115
marcuslim4299 <marcuslim4299@gmail.com>
2018-12-18 15:25:06 +0800

    golem will defend when the hp is lesser than max hp but not dead

---
+++ Assets/Marcus Assets/Scripts/GolemSlimeDefend.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemSlimeDefend : MonoBehaviour {
+
+    public Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<SlimeBase>().GetHealth() < GetComponent<SlimeBase>().m_MaxHealth && !anim.GetBool("IsDefending"))
+        {
+            anim.SetBool("IsDefending", true);
+        }
+    }
+}
