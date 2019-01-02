d7072c5b9a7cdbc992f1330ecefc88818c19d7f5
marcuslim4299 <marcuslim4299@gmail.com>
2019-01-02 16:26:28 +0800

    golem attack the wall

--- Assets/Marcus Assets/Scripts/Despawner.cs
+++ Assets/Marcus Assets/Scripts/Despawner.cs
@@ -11,18 +11,26 @@ public class Despawner : MonoBehaviour {
         {
             if (other.tag != "Enemy")
                 return;
-            
-            other.GetComponent<SlimeBase>().toDespawn = true;
-            SlimeManager.instance.GetComponent<SlimeManager>().Remove();
 
-            other.GetComponent<SlimeBase>().anim.SetBool("IsDead", true);
-
-            if (other.GetComponent<GolemSlimeDefend>() != null)
+            if (other.GetComponent<GolemSlimeDefend>() == null)
             {
-                other.GetComponent<SlimeBase>().anim.SetBool("IsDefending", false);
+                other.GetComponent<SlimeBase>().toDespawn = true;
+                SlimeManager.instance.GetComponent<SlimeManager>().Remove();
+
+                other.GetComponent<SlimeBase>().anim.SetBool("IsDead", true);
+
+                if (other.GetComponent<GolemSlimeDefend>() != null)
+                {
+                    other.GetComponent<SlimeBase>().anim.SetBool("IsDefending", false);
+                }
+
+                other.GetComponent<NavMeshAgent>().enabled = false;
             }
 
-            other.GetComponent<NavMeshAgent>().enabled = false;
+            else
+            {
+                other.GetComponentInChildren<Animator>().SetBool("IsAttack", true);
+            }
         }
     }
 }
