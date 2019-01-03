d596689e09b11a0ba9c28931d12f68bd9dd994be
Wafieqa AZ <maguu.samurai@gmail.com>
2019-01-03 13:31:43 +0800

    Dynamite WIP :,>

---
+++ Assets/Scripts/Others/Dynamite.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamite : MonoBehaviour {
+    //HI HUI SHENG- PLS CHANGE THIS TO BE BETTER IF YOU THINK IT'S NOT OPTIMAL :,DDDD th e n k  s
+
+    //TODO:
+    //Move spark down the fuse
+    //When spark reaches base of fuse:
+    //play explosion
+    //if explosion range hits enemy kill them
+
+    public ParticleSystem m_SparkParticle;
+    public ParticleSystem m_ExplosionParticle;
+    public GameObject m_DynamiteObject;
+    public GameObject m_FuseObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
