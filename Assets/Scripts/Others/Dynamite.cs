34ea0c06651cafebad71dd15c9c79a579b4e0200
Wafieqa AZ <maguu.samurai@gmail.com>
2019-01-04 17:07:18 +0800

    Dynamite update :>

--- Assets/Scripts/Others/Dynamite.cs
+++ Assets/Scripts/Others/Dynamite.cs
@@ -6,24 +6,34 @@ public class Dynamite : MonoBehaviour {
     //HI HUI SHENG- PLS CHANGE THIS TO BE BETTER IF YOU THINK IT'S NOT OPTIMAL :,DDDD th e n k  s
 
     //TODO:
-    //Move spark down the fuse
-    //When spark reaches base of fuse:
+    //Move spark down the fuse (Animation)
+    //When spark reaches base of fuse: (End of animation)
     //play explosion
     //if explosion range hits enemy kill them
 
-    public ParticleSystem m_SparkParticle;
-    public ParticleSystem m_ExplosionParticle;
+    public Animator DynamiteAnimator;
+    public GameObject m_ExplosionParticle;
     public GameObject m_DynamiteObject;
-    public GameObject m_FuseObject;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //OnTriggerEnter with torch flame
+    //DynamiteAnimator.SetBool("isLighted", true);
 
+    public void PlayExplosion()
+    {
+        //if there is slime particles
+        if (m_ExplosionParticle != null)
+        {
+            //Create temp GO and instantiate in it
+            GameObject temp = Instantiate(m_ExplosionParticle, m_DynamiteObject.transform.position, m_ExplosionParticle.gameObject.transform.rotation);
+            Destroy(temp, 3); //Destroy after 3 secs
+
+            //if(enemies nearby)
+            //{kill them}
+
+            //if(player nearby)
+            //{kill player, blackscreen}
+
+        }
+        Destroy(m_DynamiteObject); //get rid of all of them
+    }
 }
