439ff67fa9fde11427475504330b1aa3b94d7495
Wafieqa AZ <maguu.samurai@gmail.com>
2019-01-09 10:11:37 +0800

    audio slime death ^^

--- Assets/Marcus Assets/Scripts/SlimeDeath.cs
+++ Assets/Marcus Assets/Scripts/SlimeDeath.cs
@@ -21,6 +21,10 @@ public class SlimeDeath : MonoBehaviour {
     {
         if (m_SlimeBase.m_DeathParticles != null)
         {
+            AudioManager.Instance.Play3D("basicdeath", transform.position, AudioManager.AudioType.Additive, new AudioSourceData3D(){ randomPitchRange = 0.4f, volume = .5f});
+
+            AudioManager.Instance.Play3D("slimesplatter", transform.position, AudioManager.AudioType.Additive, new AudioSourceData3D() { volume = .2f }, .1f);
+
             GameObject temp = Instantiate(m_SlimeBase.m_DeathParticles, Slime.transform.position, m_SlimeBase.m_DeathParticles.gameObject.transform.rotation);
             Destroy(temp, 3);
         }
