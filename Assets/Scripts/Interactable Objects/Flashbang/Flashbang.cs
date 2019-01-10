8a4b87410abbd8274f8e818992a62faeb46841ee
HS @ FYP Lab <deathezer10@gmail.com>
2019-01-10 18:22:58 +0800

    Initial flashbang bloom

--- Assets/Scripts/Interactable Objects/Flashbang/Flashbang.cs
+++ Assets/Scripts/Interactable Objects/Flashbang/Flashbang.cs
@@ -27,7 +27,7 @@ public class Flashbang : GrabbableObject
         if (m_IsLighted == false)
             m_FlashbangRing.GetComponent<Collider>().enabled = false;
     }
-    
+
     public void StartFuse()
     {
         GameObject audio = AudioManager.Instance.Play3D("flashbangsizzle", transform.position, AudioManager.AudioType.Additive);
@@ -40,15 +40,32 @@ public class Flashbang : GrabbableObject
         yield return new WaitForSeconds(delay);
 
         // Flashbang booms
-        var cat = GetComponent<PostProcessingBehaviour>();
-        var bloomer = cat.profile.bloom.settings;
-        bloomer.bloom.threshold = Mathf.PingPong(Time.time, 1.1f);
-        bloomer.bloom.intensity = Mathf.PingPong(Time.time, 3);
-        cat.profile.bloom.settings = bloomer;
+        //var cat = GetComponent<PostProcessingBehaviour>();
+        //var bloomer = cat.profile.bloom.settings;
+        //bloomer.bloom.threshold = Mathf.PingPong(Time.time, 1.1f);
+        //bloomer.bloom.intensity = Mathf.PingPong(Time.time, 3);
+        //cat.profile.bloom.settings = bloomer;
+
+        Debug.DrawRay(transform.position, Camera.main.transform.position - transform.position, Color.yellow, 5);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, Camera.main.transform.position - transform.position, out hitInfo) && hitInfo.transform.tag == "MainCamera")
+        {
+            // TODO: Check angle and flash the player
+            Debug.Log("Hit");
+        }
 
         AudioManager.Instance.Play3D("flashbangboom", transform.position, AudioManager.AudioType.Additive);
         Instantiate(m_FlashParticle, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            StartFuse();
+        }
     }
 
 }
