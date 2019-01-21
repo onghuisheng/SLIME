ea1c26d5c2b70b2c048fef7dab0448fb4d50cbd2
marcuslim4299 <marcuslim4299@gmail.com>
2019-01-21 10:53:07 +0800

    fixed golem first hit no sound

--- Assets/Marcus Assets/Scripts/GolemAttack.cs
+++ Assets/Marcus Assets/Scripts/GolemAttack.cs
@@ -5,32 +5,25 @@ using UnityEngine;
 public class GolemAttack : MonoBehaviour {
 
     public Animator anim;
-
-    public float m_AttackTime;
-
+    
 	// Use this for initialization
 	void Start () {
-        m_AttackTime = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (anim.GetBool("IsAttack"))
-        {
-            m_AttackTime += Time.deltaTime;
+        
+    }
 
-            if (m_AttackTime > 2.0f && !anim.GetBool("IsAttacking"))
-            {
-                anim.SetBool("IsAttacking", true);
-            }
-        }
+    public void DoAttack()
+    {
+        anim.SetBool("IsAttacking", true);
     }
 
     public void ResetAttack()
     {
         anim.SetBool("IsAttacking", false);
-        m_AttackTime = 0.0f;
     }
 
     public void GolemAttackAudio() //play in event
