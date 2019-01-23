53334f53818baab00cbef1901fd746ee76048d01
marcuslim4299 <marcuslim4299@gmail.com>
2019-01-23 14:22:23 +0800

    golem walk to player when barricade destroyed

---
+++ Assets/Marcus Assets/Scripts/AttackPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using DG.Tweening;
+
+public class AttackPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null)
+        {
+            if (other.tag != "Enemy")
+                return;
+
+            other.GetComponentInChildren<Animator>().SetBool("IsAttack", true);
+            other.GetComponentInChildren<Animator>().SetBool("IsDefending", false);
+            other.transform.DOLookAt(new Vector3(transform.position.x, other.transform.position.y, transform.position.z), 1, AxisConstraint.Y);
+            other.GetComponent<NavMeshAgent>().enabled = false;
+        }
+    }
+
+}
