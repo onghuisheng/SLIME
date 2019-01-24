29ad1480290eb6fe3609af2c5393446ea0025cf2
Wafieqa AZ <maguu.samurai@gmail.com>
2019-01-24 11:35:53 +0800

    Start wave! :D (remember to check bonfire prefab :>)

--- Assets/Scripts/Interactable Objects/Bonfire.cs
+++ Assets/Scripts/Interactable Objects/Bonfire.cs
@@ -15,12 +15,46 @@ public class Bonfire : MonoBehaviour, IShootable
 
     bool isLighted = false;
 
+    bool isFirstShot = true;
+
+    public GameObject m_SpawnPointGO1;
+    public GameObject m_SpawnPointGO2;
+    public GameObject m_SpawnPointGO3;
+    public GameObject m_ArchersGO;
+    public GameObject m_CatapultGO;
+
+
     public void OnShot(ArrowBase arrow)
     {
         if ((arrow.arrowType == ArrowBase.ArrowType.Flame || arrow.arrowType == ArrowBase.ArrowType.FlameVolley) && !isLighted) //if its flame arrow
         {
             ToggleFire(true);
+
+            //If it is first time, send in the wave
+            if(isFirstShot)
+            {
+                isFirstShot = false;
+                StartWave();
+            }
         }
+    }
+
+    public void StartWave()
+    {
+        //Play horn audio
+        AudioManager.Instance.Play2D("warhorn", AudioManager.AudioType.Additive);
+
+        //set gameobjects as active
+        //this..is kinda... ew.. so please improve this :,UUU
+        if(m_SpawnPointGO1 && m_SpawnPointGO2 && m_SpawnPointGO3 && m_ArchersGO && m_CatapultGO != null)
+        {
+            m_SpawnPointGO1.SetActive(true);
+            m_SpawnPointGO2.SetActive(true);
+            m_SpawnPointGO3.SetActive(true);
+            m_ArchersGO.SetActive(true);
+            m_CatapultGO.SetActive(true);
+        }
+
     }
 
     public void ToggleFire(bool toggle)
