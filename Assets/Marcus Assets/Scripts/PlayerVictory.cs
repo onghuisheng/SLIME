76fc50b5d0f62f7699c95f7d88660da8a7396c39
marcuslim4299 <marcuslim4299@gmail.com>
2019-02-07 10:42:31 +0800

    i hope this fixes

--- Assets/Marcus Assets/Scripts/PlayerVictory.cs
+++ Assets/Marcus Assets/Scripts/PlayerVictory.cs
@@ -15,11 +15,12 @@ public class PlayerVictory : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (SlimeManager.instance.m_CurrentWave == 2 && SlimeManager.m_GameType == SlimeManager.GameType.Normal && m_Spawn == true)
+        if (SlimeManager.instance.m_CurrentWave == 2 && SlimeManager.m_GameType == SlimeManager.GameType.Normal && m_Spawn == false)
         {
             if (SlimeManager.instance.m_SlimeInScene.Count == 0 && SlimeManager.instance.m_FinishSpawnWave == true && SlimeManager.instance.m_SlimeInWave.Count == SlimeManager.instance.m_SlimeWaves[SlimeManager.instance.m_CurrentWave] && SlimeManager.instance.m_GolemSlimeInWave.Count == SlimeManager.instance.m_GolemSlimeWaves[SlimeManager.instance.m_CurrentWave])
             {
                 SlimeManager.instance.m_GameFinish = true;
+                m_Spawn = true;
                 Instantiate(m_FireWorks, m_FireWorks.transform.position, m_FireWorks.transform.rotation);
             }
         }
