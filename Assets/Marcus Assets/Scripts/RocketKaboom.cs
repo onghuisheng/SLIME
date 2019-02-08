f940b1f60ece0e45c0e0e4aae3f3a72b946e3334
marcuslim4299 <marcuslim4299@gmail.com>
2019-02-08 17:53:22 +0800

    whee boom with proper math

--- Assets/Marcus Assets/Scripts/RocketKaboom.cs
+++ Assets/Marcus Assets/Scripts/RocketKaboom.cs
@@ -6,22 +6,35 @@ public class RocketKaboom : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(PlaySound(GetComponent<ParticleSystem>().main.startLifetime.constant));
+        float nextTime = 1 / GetComponent<ParticleSystem>().emission.rateOverTime.constant;
+        float waitTime = GetComponent<ParticleSystem>().main.startLifetime.constant * nextTime;
+
+        if (waitTime > GetComponent<ParticleSystem>().main.startLifetime.constant)
+            waitTime = GetComponent<ParticleSystem>().main.startLifetime.constant;
+
+        StartCoroutine(PlaySound(waitTime, nextTime));
     }
 
     // Update is called once per frame
     void Update () {
     }
 
-    IEnumerator PlaySound(float m_WaitTime)
+    IEnumerator PlaySound(float m_WaitTime, float nextTime)
     {
         WaitForSeconds m_Time = new WaitForSeconds(m_WaitTime);
+        WaitForSeconds m_Next = new WaitForSeconds(nextTime);
 
-        while(true)
+        float temp = Mathf.Abs(nextTime - m_WaitTime);
+        WaitForSeconds m_NewNext = new WaitForSeconds(temp);
+
+        yield return m_Next;
+
+        while (true)
         {
+            AudioManager.Instance.Play3D("whee", transform.position, AudioManager.AudioType.Additive, new AudioSourceData3D() { randomPitchRange = .2f });
             yield return m_Time;
-            AudioManager.Instance.Play3D("npc_goodjob", transform.position, AudioManager.AudioType.Additive, new AudioSourceData3D() { randomPitchRange = .2f });
-
+            AudioManager.Instance.Play3D("dynexp", transform.position, AudioManager.AudioType.Additive, new AudioSourceData3D() { randomPitchRange = .2f });
+            yield return m_NewNext;
         }
     }
 }
