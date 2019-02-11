3158adfa514d58eb9f50abefe86bee9089e8f778
marcuslim4299 <marcuslim4299@gmail.com>
2019-02-11 10:50:58 +0800

    slime respawner

---
+++ Assets/Marcus Assets/Scripts/SlimeRespawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeRespawn : MonoBehaviour {
+
+    public GameObject m_Slime;
+    public GameObject m_SceneSlime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(m_SceneSlime == null)
+        {
+            m_SceneSlime = Instantiate(m_Slime, transform.position, transform.rotation);
+        }
+	}
+}
