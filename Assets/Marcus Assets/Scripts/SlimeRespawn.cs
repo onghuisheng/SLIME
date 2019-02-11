13561f1f90a3823f4d95fc9613d92be7cfefcfb1
marcuslim4299 <marcuslim4299@gmail.com>
2019-02-11 14:46:56 +0800

    added delay to respawn

--- Assets/Marcus Assets/Scripts/SlimeRespawn.cs
+++ Assets/Marcus Assets/Scripts/SlimeRespawn.cs
@@ -6,17 +6,27 @@ public class SlimeRespawn : MonoBehaviour {
 
     public GameObject m_Slime;
     public GameObject m_SceneSlime;
+    public List<GameObject> m_Waypoints;
+    private float m_Delay;
 
 	// Use this for initialization
 	void Start () {
-		
+        m_Waypoints = m_SceneSlime.GetComponent<SlimePatrol>().m_WaypointList;
+        m_Delay = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(m_SceneSlime == null)
+        if (m_SceneSlime == null)
         {
-            m_SceneSlime = Instantiate(m_Slime, transform.position, transform.rotation);
+            m_Delay += Time.deltaTime;
+
+            if (m_Delay > 2.0f)
+            {
+                m_SceneSlime = Instantiate(m_Slime, transform.position, transform.rotation);
+                m_SceneSlime.GetComponent<SlimePatrol>().m_WaypointList = m_Waypoints;
+                m_Delay = 0.0f;
+            }
         }
 	}
 }
